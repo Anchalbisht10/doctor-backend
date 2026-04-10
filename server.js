08b0b18c2af79547c7d96ccb2cc83c63b17c179e
Anchal Bisht <anchal001bisht@gmail.com>
2026-04-10 13:18:09 +0530

    update

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ app.use(cors({
     "https://doctor-frontend-sand.vercel.app"
   ],
    credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization", "token"]
 }));
 //so this is for cors code ok
 
